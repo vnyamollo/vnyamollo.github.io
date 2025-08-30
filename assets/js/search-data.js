1f38a20cbb4a2ad1d47e894ad72b627cb125d4a2
vnyamollo <nyamolovincent@gmail.com>
2025-08-30 18:16:14 +0000

    Deploying to gh-pages from @ vnyamollo/vnyamollo.github.io@48328fb7cc2fa3b44184ddbc982a6a7482118723 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-on-the-effects-of-stretching-on-students-discomfort-and-cognitive-load-is-accepted-at-the-hfes-conference-2025",
+          title: 'Our paper on the effects of stretching on students’ discomfort and cognitive load...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
