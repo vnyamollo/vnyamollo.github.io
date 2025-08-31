210174a769ea93cf7703c6c3b8c64d8104976346
vnyamollo <nyamolovincent@gmail.com>
2025-08-31 02:02:16 +0000

    Deploying to gh-pages from @ vnyamollo/vnyamollo.github.io@ab546efd0c409e13c3be6dc41bb3b1a5a644ca30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
