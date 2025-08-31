b0db10eab9d0193bc34a52e8bf6535a8d16a5120
vnyamollo <nyamolovincent@gmail.com>
2025-08-31 01:36:21 +0000

    Deploying to gh-pages from @ vnyamollo/vnyamollo.github.io@60556b3799bf057135cc1dd7b8a90839244186c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,20 +474,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/vnyamollo# your LinkedIn user name", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
