7e68358c7dba3c13b1ec4575d46d924d3073dd08
vnyamollo <nyamolovincent@gmail.com>
2025-09-25 13:50:36 +0000

    Deploying to gh-pages from @ vnyamollo/vnyamollo.github.io@10368c09446352e0113fa1ea09c756757360da78 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,11 +412,6 @@ ninja.data = [{
           description: "Predicting PM2.5 Concentration in Under-Monitored Urban Areas Using Satellite-Derived Predictors and Machine Learning",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
